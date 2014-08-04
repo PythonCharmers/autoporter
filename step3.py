c7cdbc644c7fc3b04198018152bf17ca61be0830
Charles Prosper <charlesprosper@168.131.44.138.aarnet.net.au>
2014-08-04 16:02:52 +1000

    fixed step3

--- step3.py
+++ step3.py
@@ -1,12 +1,33 @@
-(lp0
-S'https://github.com/FedericoCeratto/shoebill'
-p1
-aS'http://hubspot.github.com/sanetime/'
-p2
-aS'https://github.com/tylerlong/quick_orm'
-p3
-aS'https://github.com/trilan/snippets'
-p4
-aS'https://github.com/Kami/django-encrypted-settings'
-p5
-a.
+# get a list of packages with Github as a homepage
+
+import pickle
+import xmlrpclib
+import pprint
+client = xmlrpclib.ServerProxy('https://pypi.python.org/pypi')
+
+
+
+with open('python2only','r') as f:
+
+    py2onlylist = pickle.load(f)
+
+
+
+
+#print len(py2onlylist)
+py2onlylist = list(py2onlylist)
+
+py2gitlist =[]
+
+#print py2onlylist[0] 
+
+for i in py2onlylist:
+    rel = client.package_releases(i)
+    if rel:
+        link = client.release_data(i,rel[0])["home_page"]
+        if "github" in link:
+            py2gitlist.append(link)
+            print link
+
+with open('python2gitlist','w') as f:
+    pickle.dump(py2gitlist,f)
