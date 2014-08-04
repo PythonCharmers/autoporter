5b1710d0df3adc267b8c0866f14ca8989532713f
Chew Chee Ming <cheeming@gmail.com>
2014-08-04 21:19:50 +1000

    Add some comments

--- tests/test_package.py
+++ tests/test_package.py
@@ -9,23 +9,26 @@ def main():
     import sys
     project = sys.argv[1]
 
-    # check if there is github page
     with ciu.pypi.pypi_client() as client:
+        # get package data for project
         releases = client.package_releases(project)
         if not releases:
             print('NO releases found for {}'.format(project))
             sys.exit(1)
-
-        got_github = False
         version = releases[0]
         package_data = client.package_data(project, version)
 
+        # check if project supports py3
+        # if yes, do nothing more, exit
         classifiers = package_data['classifiers']
         for classifier in classifiers:
             if 'Programming Language :: Python :: 3' in classifier:
                 print('py3 OK')
                 sys.exit(0)
+        print('py3 NOT OK')
 
+        # check if there is github page
+        got_github = False
         r = urlparse(package_data['home_page'])
         if r.netloc == 'github.com':
             got_github = True
@@ -34,7 +37,6 @@ def main():
         else:
             print('github NOT FOUND')
 
-    print('py3 NOT OK')
     sys.exit(1)
 
 if __name__ == '__main__':
